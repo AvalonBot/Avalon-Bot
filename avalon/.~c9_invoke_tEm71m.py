a41028c2154843af00d32728598a7e7ae57bf74a
Benny <asdfghjbenny@gmail.com>
2016-10-16 23:08:50 +0800

    Delete a lot of unrelevant files. Also the token.txt file.

--- avalon/.~c9_invoke_tEm71m.py
+++
@@ -1,9 +0,0 @@
-import threading
-
-def last_sender(the_)
-
-        threading.Thread( target = last_, args=context )
-    if len(player.text_queue) == 0:
-        threading.Thread( target = last_sender, args=(player.id, context ) )
-    else:
-        player.text_queue.push[ context ]
--- avalon/.~c9_invoke_ywKAtb.py
+++
@@ -1,9 +0,0 @@
-def send_to_que
-
-def last_sender(the_)
-
-def send_to_queue( player, content ):
-    if len(player.text_queue) == 0:
-        threading.Thread( target = last_sender, args=(player.id, context ) )
-    else:
-        player.text_queue.push[ context ]
